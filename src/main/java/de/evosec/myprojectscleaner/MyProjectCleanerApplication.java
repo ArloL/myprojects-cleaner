b0d42af044470e56533417f67aca8ef13e141496
Arlo O'Keeffe <tiger@k5d.de>
2023-09-19 21:17:00 +0200

    use jakarta import

--- src/main/java/de/evosec/myprojectscleaner/MyProjectCleanerApplication.java
+++ src/main/java/de/evosec/myprojectscleaner/MyProjectCleanerApplication.java
@@ -4,7 +4,7 @@ import java.net.ProxySelector;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
